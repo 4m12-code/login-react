3b76eb9bcb54a3246bd73b6d0e58ce391f557fc6
4m12-code <112677210+4m12-code@users.noreply.github.com>
2023-11-13 13:05:46 -0500

    agragados los pantallazos de la funcionalida en desktop

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,15 +4,14 @@
 import React from 'react'
 import {Logon} from "./Logon"
 import Slider from './Slider'
-import fondo1 from "../img/fondo.jpg"
-import fondo3 from "../img/fondo2.jpg"
-import fondo4 from "../img/fondo3.jpg"
+import fondo1 from "../img/Login.png"
+import fondo3 from "../img/Registrar.png"
+import fondo4 from "../img/Búsqueda.png"
 import fondo2 from "../img/fondo4.jpg"
 
 export const Main = () => { //Imagenes para el slider
     const mockImagenes = [
 		fondo1,
-		fondo2,
 		fondo3,
         fondo4
 	];
